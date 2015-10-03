5417b81211f5b59a86b183ffe3a2422991e28841
AkhilNadendla <anadendla@g.ucla.edu>
2015-10-03 00:38:44 -0700

    added app icon and set up login page

---
+++ Humans Without Borders/Humans Without Borders/Login View Controller.swift
@@ -0,0 +1,43 @@
+//
+//  Login View Controller.swift
+//  Humans Without Borders
+//
+//  Created by Akhil Nadendla on 10/3/15.
+//  Copyright © 2015 Humans Without Borders. All rights reserved.
+//
+
+import UIKit
+
+class Login_View_Controller: UIViewController {
+
+    @IBOutlet var TitleMain: UILabel!
+    
+    @IBOutlet var TitleSecondary: UILabel!
+    
+    @IBOutlet var LoginBtn: UIButton!
+    
+    @IBOutlet var SignupBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
