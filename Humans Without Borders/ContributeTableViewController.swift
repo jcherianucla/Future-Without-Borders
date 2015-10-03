d949f365df96b117afad7de5bfc152ad3a070e5f
AkhilNadendla <anadendla@g.ucla.edu>
2015-10-03 04:08:30 -0700

    added stuff to contribute

---
+++ Humans Without Borders/ContributeTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ContributeTableViewController.swift
+//  Humans Without Borders
+//
+//  Created by Akhil Nadendla on 10/3/15.
+//  Copyright © 2015 Humans Without Borders. All rights reserved.
+//
+
+import UIKit
+
+class ContributeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return (self.view.bounds.height / 3.5)
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! ContributeTableViewCell
+        // Configure the cell...
+        cell.imageBG.image = UIImage(named: "Contribute BG \(indexPath.row+1)")
+        if indexPath.row == 0 {
+            cell.label.text = "Host a Family"
+        }
+        else if indexPath.row == 1 {
+            cell.label.text = "Donate to a Host Family"
+        }
+        else {
+            cell.label.text = "Find out more about this Crisis"
+        }
+        return cell
+    }
+    
+
+
+}
---
+++ Humans Without Borders/Humans Without Borders/ContributeTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ContributeTableViewCell.swift
+//  Humans Without Borders
+//
+//  Created by Akhil Nadendla on 10/3/15.
+//  Copyright © 2015 Humans Without Borders. All rights reserved.
+//
+
+import UIKit
+
+class ContributeTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageBG: UIImageView!
+    @IBOutlet var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
