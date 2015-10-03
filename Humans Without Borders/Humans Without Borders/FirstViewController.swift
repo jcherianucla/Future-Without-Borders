2b3b61abc20ebb0cc51cecad91b2ba561b37ccad
AkhilNadendla <anadendla@g.ucla.edu>
2015-10-02 23:49:32 -0700

    initial repo

---
+++ Humans Without Borders/Humans Without Borders/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Humans Without Borders
+//
+//  Created by Akhil Nadendla on 10/2/15.
+//  Copyright © 2015 Humans Without Borders. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Humans Without Borders/Humans Without BordersTests/Humans_Without_BordersTests.swift
@@ -0,0 +1,36 @@
+//
+//  Humans_Without_BordersTests.swift
+//  Humans Without BordersTests
+//
+//  Created by Akhil Nadendla on 10/2/15.
+//  Copyright © 2015 Humans Without Borders. All rights reserved.
+//
+
+import XCTest
+@testable import Humans_Without_Borders
+
+class Humans_Without_BordersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
